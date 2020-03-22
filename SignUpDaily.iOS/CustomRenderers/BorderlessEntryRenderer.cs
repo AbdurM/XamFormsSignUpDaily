590b5eea579a110eb0d4cf716fd4b68e089ea944
Mohd Abdur Rafay <rafayk66@gmail.com>
2020-03-23 00:38:30 +1100

    Initial commit

---
+++ SignUpDaily.iOS/CustomRenderers/BorderlessEntryRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CoreAnimation;
+using CoreGraphics;
+using CustomRenderer.iOS;
+using SignUpDaily.CustomControls;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(BorderlessEntry), typeof(BorderlessEntryRenderer))]
+namespace CustomRenderer.iOS
+{
+    public class BorderlessEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            
+            var borderLayer = new CALayer();
+            borderLayer.BorderColor = UIColor.White.CGColor;
+            borderLayer.MasksToBounds = true;
+            borderLayer.Frame = new CoreGraphics.CGRect(0f, Frame.Height / 2, Frame.Width, 1f);
+            borderLayer.BorderWidth = 1f;
+
+            Control.Layer.AddSublayer(borderLayer);
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+    }
+}
