05c20556be30333fdc5ecc30f3ef619cb13848ae
abdur <rafayk66@gmail.com>
2020-03-28 18:30:17 +1100

    Android Done

--- SignUpDaily.iOS/CustomRenderers/BorderlessEntryRenderer.cs
+++ SignUpDaily.iOS/CustomRenderers/BorderlessEntryRenderer.cs
@@ -15,14 +15,6 @@ namespace CustomRenderer.iOS
         protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
         {
             base.OnElementChanged(e);
-            
-            var borderLayer = new CALayer();
-            borderLayer.BorderColor = UIColor.White.CGColor;
-            borderLayer.MasksToBounds = true;
-            borderLayer.Frame = new CoreGraphics.CGRect(0f, Frame.Height / 2, Frame.Width, 1f);
-            borderLayer.BorderWidth = 1f;
-
-            Control.Layer.AddSublayer(borderLayer);
             Control.BorderStyle = UITextBorderStyle.None;
         }
     }
