de1de19ab46aac43ccbc0e9fe4735a5d93017fa6
abdur <rafayk66@gmail.com>
2020-03-28 17:53:06 +1100

    iOS Done

---
+++ SignUpDaily.Android/CustomRenderers/BorderlessEntryRenderer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Android.Graphics.Drawables;
+using CustomRenderer.Androidi;
+using SignUpDaily.CustomControls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(BorderlessEntry), typeof(BorderlessEntryRenderer))]
+namespace CustomRenderer.Androidi
+{
+    public class BorderlessEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                var c = Control as Android.Widget.TextView;
+              
+                Control.Background = new ColorDrawable(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
